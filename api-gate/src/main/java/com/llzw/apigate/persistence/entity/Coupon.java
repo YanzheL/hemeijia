5d9036c16e95a2d3921c8b81fd804848ae6b069b
YanzheL <lee.yanzhe@yanzhe.org>
2019-09-04 10:00:12 -0700

    remove introduction

--- api-gate/src/main/java/com/llzw/apigate/persistence/entity/Coupon.java
+++ api-gate/src/main/java/com/llzw/apigate/persistence/entity/Coupon.java
@@ -11,7 +11,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Data;
@@ -35,9 +34,6 @@ public class Coupon extends BaseEntity implements Pricable {
 
   @Column(nullable = false)
   protected String name;
-
-  @Lob
-  protected String introduction;
 
   protected float price;
 
