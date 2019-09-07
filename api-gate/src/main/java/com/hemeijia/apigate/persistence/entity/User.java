2c6a61af16964f89fe0c7497162b23f4c45f954e
YanzheL <lee.yanzhe@yanzhe.org>
2019-09-07 23:10:36 +0800

    max username length = 50

--- api-gate/src/main/java/com/hemeijia/apigate/persistence/entity/User.java
+++ api-gate/src/main/java/com/hemeijia/apigate/persistence/entity/User.java
@@ -24,6 +24,7 @@ public class User extends BaseEntity implements UserDetails {
   private static final long serialVersionUID = 1L;
 
   @Id
+  @Column(length = 50)
   protected String username;
 
   @Column(nullable = false)
